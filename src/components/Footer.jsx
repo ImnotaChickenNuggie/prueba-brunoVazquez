aa66cc3d5d68072825467af305a560a8f71da42b
Bruno <brunohdzv@gmail.com>
2025-05-07 13:18:48 -0600

    Feat: Se genera Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="text-center py-4 border-top mt-5 bg-body-tertiary">
+            <small>BR1 • 2025</small>
+        </footer>
+    );
+};
+
+export default Footer;
